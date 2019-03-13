fae63b24bc9d3bc5b84a112391348d5cf20c76f7
Dramalife <dramalife@live.com>
2019-03-13 15:23:35 +0800

    “LED闪烁，用很少的行数表示”，添加注释

--- lib_p/sample_status_chg_biling.c
+++ lib_p/sample_status_chg_biling.c
@@ -1,3 +1,9 @@
+/*
+Dramalife@live.com
+Date:2018.12~2019.2
+功能:交替输出A,B（LED闪烁）；
+重点：方法2，取反某一bit，缩短代码行数
+*/
 #include<stdio.h>
 void main(void)
 {
@@ -15,9 +21,10 @@ void main(void)
       flag |= 0x80;
       printf("B\r\n");
     }
+
     //ex.2  print:1,0,1,0...
     printf("%d\r\n",(flag>>3)&0x01);
-    flag = ((~flag)&(1<<3)) | (flag&(~(1<<3)));
+    flag = ((~flag)&(1<<3)) | (flag&(~(1<<3)));//取反指定bit | 清空指定bit
   }
   return;
 }
