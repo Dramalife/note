01a32e482ee5e5a0eeedfc781056d205168fb314
Dramalife <dramalife@live.com>
2019-03-13 15:46:37 +0800

    Update Linux_Terminal_Color_Dramalife.h

--- lib_dramalife/Linux_Terminal_Color_Dramalife.h
+++ lib_dramalife/Linux_Terminal_Color_Dramalife.h
@@ -1,4 +1,9 @@
-/*source url : https://www.cnblogs.com/clover-toeic/p/4031618.html*/
+/*******************************************************
+source url: 
+  https://www.cnblogs.com/clover-toeic/p/4031618.html
+sample:
+  printf(L_RED "hello,var:%d\r\n" NONE, var);
+*******************************************************/
 #define NONE                 "\e[0m"
 #define BLACK                "\e[0;30m"
 #define L_BLACK              "\e[1;30m"
