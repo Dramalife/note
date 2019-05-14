b85b17cab1b0ef31070f8fcd63bdf86a08b1bbaa
dramalife@live.com <dramalife@live.com>
2019-05-14 19:58:51 +0800

    [git,svn] sort out files

---
+++ tools/coding/git.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+# 1. Base Configuration
+# --------
+# --system : all user(local machine)
+# --golbal : current user
+# --local  : current path
+# --list   : show current cfg
+git config --list
+git config --global user.name "Dramalife"
+git config --global user.email dramalife@live.com
+
+# 2. Create an empty Git repository or reinitialize an existing one
+# --------
+#git init
+
+# 3. Clone a respository
+# --------
+# 3.1 remote copy
+# 	git clone https://github.com/Dramalife/note.git
+# 3.2 local copy .step1
+# 	git clone ../note/ ./note/
+# 	git remote set-url origin https://github.com/Dramalife/note.git
+
+# 4. Status
+# ______________________________________________________________________________________________________
+# |Status				|ahead				|back				|
+# |-------------------------------------|-------------------------------|-------------------------------|
+# |Untracked files			|git add <file>		  	|git checkout <file>		|
+# |-------------------------------------|-------------------------------|-------------------------------|
+# |Changes to be committed		|git commit -m"xx" <file>/-a  	|git revert			|
+# |-------------------------------------|-------------------------------|-------------------------------|
+# |					|git push		 	|				|
+# |-------------------------------------|-------------------------------|-------------------------------|
+# |					|git reset HEAD <file>	 	|				|
+# ```````````````````````````````````````````````````````````````````````````````````````````````````````
+
+
+# Changes not staged for commit:
+#  	(use "git add <file>..." to update what will be committed)
+#   	(use "git checkout -- <file>..." to discard changes in working directory)#####################git check file
+#    	modified:   test2/test2.txt
+# Your branch is ahead of 'origin/master' by 1 commit.(use "git push" to publish your local commits)
+
+git status
