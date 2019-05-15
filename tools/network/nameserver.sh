0811bfc4931f92aa0b538b6031a013b726848ecb
Dramalife <dramalife@live.com>
2019-05-15 14:57:08 +0000

    [tools-network] nameserver.sh

--- tools/network/nameserver.sh
+++ tools/network/nameserver.sh
@@ -1,2 +1,15 @@
-echo "nameserver 114.114.114.114" >> /etc/resolvconf/resolv.conf.d/base
-#dns-nameservers 8.8.8.8 ->  /etc/network/interfaces
+#root@NETAC-SERVER:~/ # vi /etc/resolvconf/resolv.conf.d/base 
+#root@NETAC-SERVER:~/ # vi /etc/resolvconf/resolv.conf.d/head
+
+echo "clean.."
+mv /etc/resolvconf/resolv.conf.d/base /etc/resolvconf/resolv.conf.d/base_bk$(date "+%Y%m%d")
+mv /etc/resolvconf/resolv.conf.d/head /etc/resolvconf/resolv.conf.d/head_bk$(date "+%Y%m%d")
+
+echo "dns.."
+echo "nameserver 192.168.0.1" > /etc/resolvconf/resolv.conf.d/head
+echo "nameserver 114.114.114.114" > /etc/resolvconf/resolv.conf.d/base
+
+
+echo "route.."
+route add default gw 192.168.0.1
+
