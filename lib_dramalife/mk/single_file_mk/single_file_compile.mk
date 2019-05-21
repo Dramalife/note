995de4a61172ac91088fa0eb3eaf34a075dbf832
dramalife@live.com <dramalife@live.com>
2019-05-21 09:54:54 +0800

    update

--- lib_dramalife/mk/single_file_mk/single_file_compile.mk
+++ lib_dramalife/mk/single_file_mk/single_file_compile.mk
@@ -1,4 +1,5 @@
 # update : 2019.05.13
+# update : 2019.05.20 add gcc -v
 
 BIN_NAME=a_demo
 BIN_END=.out
@@ -7,7 +8,7 @@ LIB_C_DRAMALIFE=~/note/lib_dramalife/
 
 all:
 	rm -rvf ./*$(BIN_END)
-	gcc -o $(BIN_NAME)$(BIN_END) $(SRC) -I $(LIB_C_DRAMALIFE)	
+	gcc -o $(BIN_NAME)$(BIN_END) $(SRC) -I $(LIB_C_DRAMALIFE) -v
 
 	# "make: *** [all] Error 1" 
 	./$(BIN_NAME)$(BIN_END)
