318b9fc759ead16f802a5861e24cd9d01dc3e90f
dramalife@live.com <dramalife@live.com>
2019-05-23 16:20:07 +0800

    [git]git clean untracked

--- tools/coding/git.sh
+++ tools/coding/git.sh
@@ -47,4 +47,5 @@ git config --global user.email dramalife@live.com
 
 git status
 # git status -uno #Show no untracked files
+# git clean -d -n #Del untracked files(-n just show, -i really clean!!)
 
