126162e53c42d1f0547fdaa747e34c5ae36d1a48
dramalife@live.com <dramalife@live.com>
2019-05-24 14:19:08 +0800

    [ lib-shell ] , install , Ref

---
+++ lib_dramalife/install_sh_lib_to_home.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+
+#DES_PATH_TO_INSTALL="~/.note_wjy_lib_sh"
+DES_PATH_TO_INSTALL="./sssss"
+
+
+# CMD_W_MKDIR="mkdir ${DES_PATH_TO_INSTALL}"
+# CMD_W_CP_SH="cp ./*.sh ${DES_PATH_TO_INSTALL}/"
+# 
+# echo ${CMD_W_MKDIR}
+# echo ${CMD_W_CP_SH}
+# 
+# exec ${CMD_W_MKDIR}
+# exec ${CMD_W_CP_SH}
+
+mkdir ~/.note_wjy_lib_sh
+cp ./terminal_color_bash.sh ~/.note_wjy_lib_sh/
+
+
