9c45194a2fcf8312e789d898412bccec2f0936c6
Dramalife <dramalife@live.com>
2019-05-26 11:26:05 +0000

    add owner info

--- tools/snmp_walk.sh
+++ tools/snmp_walk.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
+
+# Dramalife@live.com
+# 2019.05.14
+
 snmpwalk -v 1 -c public 192.168.1.1 iso.3.6.1.4.1.8886.28.3.6.1
 
--- tools/who_is_printing.sh
+++ tools/who_is_printing.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Dramalife@live.com
+# 2019.05.24
+
 source ~/.note_wjy_lib_sh/*
 
 echo "input device path,ex:\"/dev/console\""
