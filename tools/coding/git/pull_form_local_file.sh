35f55326e14fdf2b31db8f394ad496faebdde22b
dramalife@live.com <dramalife@live.com>
2019-06-28 19:35:52 +0800

    [git] pull form local file

---
+++ tools/coding/git/pull_form_local_file.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# note git-pull related file
+# Copyright (C) 2019 Dramalife
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#
+git config branch.master.remote ~/note
+
