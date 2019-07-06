2ddd1e3d34628a23ed9ccc2b62554590c3b3cafd
Dramalife(wjy) <Dramalife@live.com>
2019-07-06 13:23:45 +0800

    bit_64_32

---
+++ 80-userspace_programming/33-datastruct/link_list/gcc_related_show.c
@@ -0,0 +1,31 @@
+/*
+   note debug print related file
+   Copyright (C) 2019 Dramalife@live.com
+
+   This file is part of [note](https://github.com/Dramalife/note.git)
+
+   note is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#include <stdio.h>
+#include "gcc_related_show.h"
+
+/*
+No arg in,such as "__LINE__"
+*/
+void gcc_related_show_no_arg_in(unsigned int options)
+{
+	if( options & GCC_R_SHOW_VERSION)
+		printf("GCC VER:(%s)",__VERSION__);
+	if( options & GCC_R_SHOW_DATE)
+		printf(" DATE:(%s)",__DATE__);
+	if( options & GCC_R_SHOW_TIME)
+		printf(" TIME:(%s)",__TIME__);
+
+
+	printf("\n");
+	return;
+}
+
