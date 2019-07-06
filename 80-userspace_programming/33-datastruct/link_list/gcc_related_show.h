3b0cb7f64afc28f52a34d932f2a7b28aa6f9297d
Dramalife(wjy) <Dramalife@live.com>
2019-07-06 13:23:45 +0800

    bit_64_32

---
+++ 80-userspace_programming/33-datastruct/link_list/gcc_related_show.h
@@ -0,0 +1,15 @@
+/*gcc_related_show.h*/
+#ifndef GCC_RELATED_SHOW_H
+#define GCC_RELATED_SHOW_H
+
+#define GCC_R_SHOW_MAX		0X80000000
+#define GCC_R_SHOW_VERSION	0X40000000
+#define GCC_R_SHOW_DATE		0X20000000
+#define GCC_R_SHOW_TIME		0X10000000
+
+#define GCC_R_SHOW_MIN		0X1
+
+void gcc_related_show_no_arg_in(unsigned int options);
+
+#endif
+
