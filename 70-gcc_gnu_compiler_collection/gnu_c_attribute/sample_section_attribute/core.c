fcef11dfc8eca370a7bd7048b5c3c75e9ebdc84b
dramalife@live.com <dramalife@live.com>
2019-07-09 15:33:35 +0800

    add warning info.

--- 70-gcc_gnu_compiler_collection/gnu_c_attribute/sample_section_attribute/core.c
+++ 70-gcc_gnu_compiler_collection/gnu_c_attribute/sample_section_attribute/core.c
@@ -1,25 +1,60 @@
-#include<stdio.h>
+/*
+   FILE : core.c
 
-typedef void (*init_call)(void);
+   note gcc attribute-section related file
+   Copyright (C) 2019 Dramalife@live.com
+
+   This file is part of [note](https://github.com/Dramalife/note.git)
+
+   note is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+   COPY & MODIFY FROM:
+   https://mp.weixin.qq.com/s?__biz=MzAwMDUwNDgxOA==&mid=2652663356&
+   idx=1&sn=779762953029c0e0946c22ef2bb0b754&chksm=810f28a1b678a1b74
+   7520ba3ee47c9ed2e8ccb89ac27075e2d069237c13974aa43537bff4fba&
+   mpshare=1&scene=1&srcid=0111Ys4k5rkBto22dLokVT5A&pass_ticket=
+   bGNWMdGEbb0307Tm%2Ba%2FzAKZjWKsImCYqUlDUYPZYkLgU061qPsHFESXlJj%
+   2Fyx3VM#rd
+*/
+
+
+#include<stdio.h>
+#include <terminal_color_dramalife.h>/* Installed from "note/lib_dramalife" */
+
+#define AC_NONE         CNONE
+#define AC_LRED         CLRED
+#define AC_LGREEN       CLGREEN
+#define AC_YELLOW       CYELLOW
+
+typedef void (*INIT_CALL)(void);
 
 /*
  * These two variables are defined in link script.
  */
-extern init_call _init_start;
-extern init_call _init_end;
+extern INIT_CALL _init_start;
+extern INIT_CALL _init_end;
 
-void do_initcalls(void)
+void modules_init(void)
 {
-	init_call *init_ptr = &_init_start;
-	for (; init_ptr < &_init_end; init_ptr++) {
-		printf("init address: %p\n", init_ptr);
+	INIT_CALL *init_ptr = &_init_start;
+	for (; init_ptr < &_init_end; init_ptr++)
+	{
+#if  __WORDSIZE ==  64
+		printf("[%4d],"AC_LGREEN"addr"AC_NONE":(0x%016llX)\n" ,
+				__LINE__,(long long unsigned int)init_ptr);
+#elif  __WORDSIZE ==  32
+		printf("[%4d],"AC_LGREEN"addr"AC_NONE":(0x%08X)   \n" ,
+				__LINE__,(unsigned int)init_ptr);
+#endif
 		(*init_ptr)();
 	}
 }
 
 int main(void)
 {
-	do_initcalls();
+	modules_init();
 	return 0;
 }
 
--- 70-gcc_gnu_compiler_collection/gnu_c_attribute/sample_section_attribute/make.sh
+++ 70-gcc_gnu_compiler_collection/gnu_c_attribute/sample_section_attribute/make.sh
@@ -1,18 +1,39 @@
 #! /bin/bash
+#
+# note gcc attribute-section related file
+# Copyright (C) 2019 Dramalife@live.com
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
 
 SCRIPT=x86.lds
 PROGNAME=a_demo.out
 
 case $1 in
+
+# make all
 a|all)
 gcc -c *.c
 gcc -o${PROGNAME}  *.o -T ${SCRIPT}
 ;;
 
+# make clean
 c|clean)
 rm -rvf *.o
 rm -rvf *.out
 ;;
+
+# read section headers
+S|section-headers)
+gcc -c *.c
+readelf -S *.o ${PROGNAME}
+;;
+
+# help
 *)
 echo "em..."
 ;;
