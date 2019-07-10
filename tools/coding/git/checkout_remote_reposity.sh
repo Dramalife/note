22dad83236bfd45e33866100f12307779e907f7f
dramalife@live.com <dramalife@live.com>
2019-07-10 17:59:47 +0800

    [tools-git] checkout_remote_reposity.sh

---
+++ tools/coding/git/checkout_remote_reposity.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+# note tools git related file
+# Copyright (C) 2019 Dramalife
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+# {remote_name}/{remote_branch_name}
+git checkout -t origin/$1
+
