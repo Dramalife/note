293830b0592f8966b7eb0437c7ef882832b9ec76
dramalife@rc_ubuntu_vdev_x86 <dramalife@live.com>
2019-07-13 11:12:27 +0800

    [tools-soft_tools] script for getting

---
+++ tools/get_soft_tools.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# note xxx-xxx related file
+# Copyright (C) 2019 Dramalife
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+https://github.com/Dramalife/tools_note.git
+
