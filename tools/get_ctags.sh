46dcc50eef6f245fca87b1047200eab269cace52
Dramalife(wjy) <Dramalife@live.com>
2019-07-18 22:56:41 +0800

    [tools-soft] Readd soft-tools {busybox,ctags,lsof,quagga,strace5} in a light way.

---
+++ tools/get_ctags.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+# note tools related
+# Copyright (C) 2019 Dramalife@live.com
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+# Getting ctags-5.8
+../lib_dramalife/internal_used/sparse_clone_symbol.sh ctags-5.8
+
