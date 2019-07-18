6eef937c99e997e96b7c9d5bb99482e55a65bf5d
Dramalife(wjy) <Dramalife@live.com>
2019-07-18 22:56:41 +0800

    [tools-soft] Readd soft-tools {busybox,ctags,lsof,quagga,strace5} in a light way.

---
+++ tools/get_lsof.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+# note tools related
+# Copyright (C) 2019 Dramalife@live.com
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+# Getting lsof-4.86+dfsg.orig
+../lib_dramalife/internal_used/sparse_clone_symbol.sh lsof-4.86+dfsg.orig
+
