26e08c7108edd94908dcc8f06ec7e8d70ed0afcd
dramalife@rc_ubuntu_vdev_x86 <dramalife@live.com>
2019-07-19 09:56:58 +0800

    [tools-dhcp] rename.

--- tools/dhcp/refresh_dhcp.sh
+++ tools/dhcp/refresh_dhcp.sh
@@ -13,13 +13,13 @@
 # 2019.06.05
 
 
-kill -9 $(pidof dhclient)
-ifconfig eth1 down
-ifconfig eth1 up
+sudo kill -9 $(pidof dhclient)
+sudo ifconfig ${1} down
+sudo ifconfig ${1} up
 echo "if restart done, press"
 read
-#tcpdump -i eth1 -w /home/baohua/sharehahaha/dhcp.pcapng &
-echo "" > /var/lib/dhcp/dhclient.leases
-dhclient eth1
+#tcpdump -i ${1} -w /home/baohua/sharehahaha/dhcp.pcapng &
+sudo echo "" > /var/lib/dhcp/dhclient.leases
+sudo dhclient ${1}
 cat /var/lib/dhcp/dhclient.leases
 
