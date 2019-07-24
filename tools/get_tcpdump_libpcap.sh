a54ffe0fadc6c3e74af6ef9f1c81217ea78101bf
dramalife@rc_ubuntu_vdev_x86 <dramalife@live.com>
2019-07-24 14:03:39 +0800

    [git-sparse] fix common; add tcpdump_libpcap

--- tools/get_tcpdump_libpcap.sh
+++ tools/get_tcpdump_libpcap.sh
@@ -11,17 +11,31 @@
 # Ref Url : http://blog.csdn.net/xuyaqun/article/details/49275477
 #
 # Init	: 2019.07.14 16:37
-# Update: 2019.07.24 14:00 Rmove "touch tool_name"
 # Update: 
+#	Init : 2019.07.24 COPY from
+#		../lib_dramalife/internal_used/sparse_clone_symbol.sh
+#		Specific Modified.
+#	Update:
+#	
 # 
 
+#FLODR=${1}
+#CONT1=${2}
+#CONT2=${3}
+FLODR=tcpdump_libpcap
+CONT1=tcpdump-4.9.2
+CONT2=libpcap-1.9.0
+
 ## step 0
-sed -i "/${1}/d" ./.gitignore
-echo "${1}" >> ./.gitignore
+touch ./.gitignore
+sed -i "/${CONT1}/d" ./.gitignore
+sed -i "/${CONT2}/d" ./.gitignore
+echo "${CONT1}" >> ./.gitignore
+echo "${CONT2}" >> ./.gitignore
 
 ## step 1
-mkdir $1
-cd $1
+mkdir ${FLODR}
+cd ${FLODR}
 git init
 
 ## step 2
@@ -29,7 +43,8 @@ git remote add -f origin https://github.com/Dramalife/tools_note.git
 
 ## step 3
 git config core.sparsecheckout true
-echo "${1}" >> .git/info/sparse-checkout
+echo "${CONT1}" >> .git/info/sparse-checkout
+echo "${CONT2}" >> .git/info/sparse-checkout
 
 ## step 4
 git pull origin master  
