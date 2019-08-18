50cda1ea2cd3132518f9431d1cc4a09cff63feea
rpi_omv <dramalife@live.com>
2019-08-18 03:22:14 +0000

    rpi LCD show

---
+++ tools/lcd_show_pic.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# note fb-lcd related file
+# Copyright (C) 2019 Dramalife
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+fbi -d /dev/fb1 -T 1 -noverbose -a disk-ug2t/cloud_disk_shared/Redmi5Plus/PHOTO/DCIM/1538310652480.jpg
+
