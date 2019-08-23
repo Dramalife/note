b5fa718241abe95fbc18fc85d3b4d454a19c504f
rc_ubuntu_vdev_x86 <dramalife@live.com>
2019-08-23 17:38:07 +0800

    Add libMakefile, Update sample_single_file.mk and gen_lib_so_step_by_step.txt .

---
+++ lib_dramalife/libMakefile.mk
@@ -0,0 +1,8 @@
+BIN_NAME=a_demo
+
+BIN_END=.out
+BIN_O=.o
+PRE_COMP=.i
+DIS_ASSB=.ass
+SHD_LIB=.so
+
--- lib_dramalife/mk/single_file_mk/sample_single_file.mk
+++ lib_dramalife/mk/single_file_mk/sample_single_file.mk
@@ -1,6 +1,6 @@
 #
-# note make related "makefile"
-# Copyright (C) 2019 Dramalife
+# note "make" related file
+# Copyright (C) 2019 Dramalife@live.com
 # 
 # This file is part of [note](https://github.com/Dramalife/note.git)
 # 
@@ -8,6 +8,8 @@
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
+# $ make --version
+# TODO;
 #
 # update : 2019.05.13
 # update : 2019.05.20 add gcc -v
@@ -16,21 +18,27 @@
 #			can be installed by running 
 #			"note/lib_dramalife/install_sh_h_to_root_dir.sh";
 # update : 2019.07.11 Del '-I' option;
+# update : 2019.08.23 Add "make --version";
+#			Use lib "libMakefile.mk" ;
+#
+# Init : COPY FROM "sample_single_file.mk", DATE : TODO;
+# Update 
 #
 
-BIN_NAME=a_demo
-BIN_END=.out
-PRE_COMP=source.i
+# TODO : Update "PATH_ABS";
+PATH_ABS=../
+include $(PATH_ABS)/libMakefile.mk
+
+PRE_SOURCE=source_pre$(PRE_COMP)
 SRC=$(wildcard ./*.c)
 
 all:
 	rm -rvf ./*$(BIN_END)
-	gcc -E $(SRC) >> $(PRE_COMP)
+	gcc -E $(SRC) >> $(PRE_SOURCE)
 	gcc -o $(BIN_NAME)$(BIN_END) $(SRC) -v
 
-# "make: *** [all] Error 1" 
-#./$(BIN_NAME)$(BIN_END)
 
 clean:
-	rm -rvf ./*$(BIN_END) ./$(PRE_COMP)
+	rm -rvf ./*$(BIN_END) ./*$(PRE_COMP) ./*$(BIN_O)
 
+.PHONY: clean
