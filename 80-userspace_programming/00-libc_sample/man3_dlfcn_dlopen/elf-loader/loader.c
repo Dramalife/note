c0f62e79addef5c53667792cacad81136e3ded6e
rc_ubuntu_vdev_x86 <dramalife@live.com>
2019-08-29 19:34:10 +0800

    add warning.

--- 80-userspace_programming/00-libc_sample/man3_dlfcn_dlopen/elf-loader/loader.c
+++ 80-userspace_programming/00-libc_sample/man3_dlfcn_dlopen/elf-loader/loader.c
@@ -1,3 +1,27 @@
+/*
+ * note "elf-loader" related file
+ * Copyright (C) 2019 Dramalife@live.com
+ * 
+ * This file is part of [note](https://github.com/Dramalife/note.git)
+ * 
+ * note is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * $ gcc --version
+ * gcc-4.8.real (Ubuntu 4.8.5-4ubuntu8~14.04.2) 4.8.5
+ * Copyright (C) 2015 Free Software Foundation, Inc.
+ * This is free software; see the source for copying conditions.  There is NO
+ * warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * $ uname -a
+ * Linux ubuntu 4.0.0-040000-generic #201504121935 SMP Sun Apr 12 23:58:08 UTC 2015 i686 i686 i686 GNU/Linux;
+ * 
+ * Init : 2019.08.29, COPY FROM : https://github.com/21cnbao/training  ;
+ * Update 
+ *
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <libelf.h>
@@ -147,3 +171,4 @@ int main(int argc, char** argv, char** envp)
 	ptr=image_load(buf, sizeof buf);
 	return ptr(argc,argv,envp);
 }
+
