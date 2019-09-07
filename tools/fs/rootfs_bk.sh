ffc99aba37d9c98cfabb0e36f3ff953cf66f8684
rpi_omv <dramalife@live.com>
2019-09-07 12:16:52 +0000

    shell : backup rootfs.

---
+++ tools/fs/rootfs_bk.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+#
+# note "rootfs-backup" related file
+# Copyright (C) 2019 Dramalife@live.com
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# bash --version
+# GNU bash, version 4.4.12(1)-release (arm-unknown-linux-gnueabihf)
+# Copyright (C) 2016 Free Software Foundation, Inc.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+# 
+# This is free software; you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+# ;
+#
+# Init : 2019.09.07;
+# Update :
+#
+
+
+if [ ${#} -gt 0 ]; then
+BK_OUTPUT_FILE=${1}
+else
+BK_OUTPUT_FILE=./$(date +%Y%m%d%H).tgz
+fi
+
+nohup tar zcvfp ${BK_OUTPUT_FILE} \
+/* \
+--exclude=/proc \
+--exclude=/lost+found \
+--exclude=/${BK_OUTPUT_FILE} \
+--exclude=/mnt \
+--exclude=/sys \
+--exclude=/sharedfolders \
+--exclude=/srv \
+&
+
