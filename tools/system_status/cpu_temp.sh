ace1c926c3f90e8222aaf3b537d9420b4009ac27
dramalife <dramalife@live.com>
2019-09-15 16:05:11 +0800

    lscpu, cpu_tempetature.

---
+++ tools/system_status/cpu_temp.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#
+# note "cpu tempature" related file
+# Copyright (C) 2019 Dramalife@live.com
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# bash --version
+# GNU bash, version 4.4.12(1)-release (arm-unknown-linux-gnueabihf)
+# Copyright (C) 2016 Free Software Foundation, Inc.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+# 
+# This is free software; you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+# ;
+#
+# Init : 2019.09.14, Ref Url : https://www.cnblogs.com/super-age/p/7163814.html;
+# Update :
+#
+
+
+cat /sys/class/thermal/thermal_zone0/temp
