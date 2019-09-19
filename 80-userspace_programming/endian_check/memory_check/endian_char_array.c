2f2137b7f43465ac18d4394a90a583cec7cf8ae0
rc_ubuntu_vdev_x86 <dramalife@live.com>
2019-09-19 16:20:07 +0800

    mistake ?

--- 80-userspace_programming/endian_check/memory_check/endian_char_array.c
+++ 80-userspace_programming/endian_check/memory_check/endian_char_array.c
@@ -7,10 +7,10 @@ void test_endian(void)
 	/* objdump:
 	 * 804847e:       c7 45 f0 6c 6c 62 62    movl   $0x62626c6c,-0x10(%ebp)   
 	 */
-	char strrrrrrrr[4]="llbb";
+	char str[4]="llbb";
 
 	/*RESULT : little endian ,l=0x6C,b=0x62*/
-	printf("%s endian ,l=0x%X,b=0x%X\n", strrrrrrrr[0] == 'l' ? "little" : "big", 'l', 'b' );
+//	printf("%s endian ,l=0x%X,b=0x%X\n", str[0] == 'l' ? "little" : "big", 'l', 'b' );
 
 	return ;
 }
