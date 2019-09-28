7697630d8c840cd4957c893f4566caad2c993395
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2019-09-28 08:54:19 +0000

    link list : Test compatible of linklist-v1.0.0 and linklist-v1.0.1. --append.

--- 80-userspace_programming/33-datastruct/link_list/main.c
+++ 80-userspace_programming/33-datastruct/link_list/main.c
@@ -35,28 +35,28 @@ int main(void)
 
 	gcc_related_show_no_arg_in( GCC_R_SHOW_VERSION | GCC_R_SHOW_DATE | GCC_R_SHOW_TIME );
 
-	head = add_if_down2up(NULL, NULL, 0);/* Init a linklist */
-	debug_if_down2up(head);//should not print
+	head = add_elem_dlll(NULL, NULL, 0);/* Init a linklist */
+	debug_dlll(head);//should not print
 	printf("LINE:%d................\n",__LINE__);
 
-	/*head =*/ add_if_down2up(head,"aaaa", 1111);/* Add item to the linklist */
-	debug_if_down2up(head);
+	/*head =*/ add_elem_dlll(head,"aaaa", 1111);/* Add item to the linklist */
+	debug_dlll(head);
 	printf("LINE:%d................\n",__LINE__);
 #if 0
-	/*head =*/ add_if_down2up(head,"bbbb", 2222);
-	debug_if_down2up(head);
+	/*head =*/ add_elem_dlll(head,"bbbb", 2222);
+	debug_dlll(head);
 	printf("LINE:%d................\n",__LINE__);
 #endif
-	chg_if_down2up(head,"aaaa",3333);/* Change value of item of the linklist */
-	debug_if_down2up(head);
+	chg_elem_dlll(head,"aaaa",3333);/* Change value of item of the linklist */
+	debug_dlll(head);
 	printf("LINE:%d................\n",__LINE__);
 
-	chg_if_down2up(head,"aaaa",-1);/* Delete item of the linklist */
-	debug_if_down2up(head);
+	chg_elem_dlll(head,"aaaa",-1);/* Delete item of the linklist */
+	debug_dlll(head);
 	printf("LINE:%d................\n",__LINE__);
 
-	/*head = */add_if_down2up(head,"aaaa", 666);
-	debug_if_down2up(head);
+	/*head = */add_elem_dlll(head,"aaaa", 666);
+	debug_dlll(head);
 	printf("LINE:%d................\n",__LINE__);
 
 	/* Add some items */
@@ -65,19 +65,19 @@ int main(void)
 	{
 		char nameaaa[20];
 		sprintf(nameaaa,"eman%d",i);
-		add_if_down2up(head,nameaaa, i);
+		add_elem_dlll(head,nameaaa, i);
 	}
-	debug_if_down2up(head);/* Print out all item(s) */
+	debug_dlll(head);/* Print out all item(s) */
 
-	if( 0 == find_by_fifo_down2up(head, &tmp) )
+	if( 0 == find_fifo_dlll(head, &tmp) )
 		printf("FIFO---name:%s,type:%d\n",tmp.name, tmp.address_type);
-	debug_if_down2up(head);
+	debug_dlll(head);
 
-	if( 0 == find_by_filo_down2up(head, &tmp) )
+	if( 0 == find_filo_dlll(head, &tmp) )
 		printf("FILO---name:%s,type:%d\n",tmp.name, tmp.address_type);
-	debug_if_down2up(head);
+	debug_dlll(head);
 
-//test_func_down2up(head, 60000, 0);/* testing */
+//test_dlll(head, 60000, 0);/* testing */
 
 	return 0;
 }
