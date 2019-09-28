ae87dd2341bb2e132d62349945bd4fb522b0646a
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2019-09-28 06:43:31 +0000

    [netplan] static addresses & dhcp mode.

---
+++ 90-operating_system/etc/netplan/netplay_apply.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+#
+# note "netplan" related file
+# Copyright (C) 2019 Dramalife@live.com
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# bash --version
+# GNU bash, version 4.4.20(1)-release (x86_64-pc-linux-gnu)
+# Copyright (C) 2016 Free Software Foundation, Inc.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+# 
+# This is free software; you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+# ;
+#
+# Init : 2019.09.28;
+# Update :
+#
+
+
+
+# Test first
+sudo netplan try
+# OR
+# Apply now
+#sudo netplan apply
