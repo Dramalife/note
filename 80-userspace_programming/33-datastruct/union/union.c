c5238e0cbab88e6c8914b71e49603a80373094d8
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2019-10-12 09:19:07 +0000

    [33-DataStruct-Union] Add WARNING.

--- 80-userspace_programming/33-datastruct/union/union.c
+++ 80-userspace_programming/33-datastruct/union/union.c
@@ -1,4 +1,41 @@
-//ref : https://www.runoob.com/cprogramming/c-unions.html
+/*
+ * note "datastruct - union" related file
+ * Copyright (C) 2019 Dramalife@live.com
+ * 
+ * This file is part of [note](https://github.com/Dramalife/note.git)
+ * 
+ * note is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * $ gcc --version
+ * gcc (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0
+ * Copyright (C) 2017 Free Software Foundation, Inc.
+ * This is free software; see the source for copying conditions.  There is NO
+ * warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * ;
+ * $ ./mips-linux-uclibc-gcc --version
+ * mips-linux-uclibc-gcc (GCC) 4.3.3
+ * Copyright (C) 2008 Free Software Foundation, Inc.
+ * This is free software; see the source for copying conditions.  There is NO
+ * warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * ;
+ *
+ * $ uname -a
+ * Linux host 2.6.31 #2 Mon Aug 12 11:09:22 CST 2019 mips GNU/Linux
+ * ;
+ * $ uname -a
+ * Linux server 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
+ * ;
+ * 
+ * REF URL : https://www.runoob.com/cprogramming/c-unions.html
+ *
+ * Init : 2019.10.12;
+ * Update 
+ *
+ */
+
+
 
 #include <stdio.h>
 #include <string.h>
@@ -13,7 +50,6 @@ union Data
 };
 
 
-
 /* FUNC : "int endian_test(void)";
  * a.out: ELF 32-bit MSB executable, MIPS, MIPS32 rel2 version 1 (SYSV), dynamically linked, interpreter /lib/ld-, with debug_info, not stripped;
  * a.out: ELF 64-bit LSB shared object, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib64/l, 
@@ -23,7 +59,7 @@ int endian_test(void)
 {
 	union Data data;
 
-	printf("\n\n\n[%s,%d]vvvvv \n",__func__,__LINE__);
+	printf("\n\n[%s,%d]vvvvv \n",__func__,__LINE__);
 
 	data.i= 0x01020304;
 	if( data.str[0] == 0x01 )
@@ -46,7 +82,7 @@ int size_union(void)
 {
 	union Data data;        
 
-	printf("\n\n\n[%s,%d]vvvvv \n",__func__,__LINE__);
+	printf("\n\n[%s,%d]vvvvv \n",__func__,__LINE__);
 
 #if (__SIZE_WIDTH__ == 64)
 	printf( "Memory size occupied by data : %ld\n", sizeof(data));
@@ -61,7 +97,7 @@ int value_set_overwrite(void)
 {
 	union Data data;        
 
-	printf("\n\n\n[%s,%d]vvvvv \n",__func__,__LINE__);
+	printf("\n\n[%s,%d]vvvvv \n",__func__,__LINE__);
 
 	data.i = 10;
 	data.f = 220.5;
@@ -78,7 +114,7 @@ int value_set(void)
 {
 	union Data data;        
 
-	printf("\n\n\n[%s,%d]vvvvv \n",__func__,__LINE__);
+	printf("\n\n[%s,%d]vvvvv \n",__func__,__LINE__);
 
 	data.i = 10;
 	printf( "data.i : %d\n", data.i);
