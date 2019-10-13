be3a5c85f3d62e6205c6e43be5f267c54dd1408c
dramalife@dl_opi_armv7 <dramalife@live.com>
2019-10-13 23:04:40 +0800

    Add options : Split or Combine compile.

--- 80-userspace_programming/33-datastruct/struct/config.mk
+++ 80-userspace_programming/33-datastruct/struct/config.mk
@@ -1,1 +1,5 @@
-PATH_ABS=../../../lib_dramalife/
+PATH_ABS:=../../../lib_dramalife/
+#SRCS := $(wildcard $(CURRENT_DIR)*.c)
+EXTERA_FILES2DEL:=./build/* ./obj/*
+include $(PATH_ABS)libMakefile_split.mk
+CFLAGS +="-D XXX=1"
