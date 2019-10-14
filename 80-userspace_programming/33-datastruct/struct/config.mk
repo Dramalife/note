88e6b7ead158c698f16923baf36760301dd470ac
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2019-10-14 02:18:44 +0000

    [33-struct] Use "lib_multi_files_mk-0.0.1" .

--- 80-userspace_programming/33-datastruct/struct/config.mk
+++ 80-userspace_programming/33-datastruct/struct/config.mk
@@ -1,5 +1,5 @@
 PATH_ABS:=../../../lib_dramalife/
 #SRCS := $(wildcard $(CURRENT_DIR)*.c)
 EXTERA_FILES2DEL:=./build/* ./obj/*
-include $(PATH_ABS)libMakefile_split.mk
+include $(PATH_ABS)mk/multi_files_mk/lib_split_make.mk
 CFLAGS +="-D XXX=1"
