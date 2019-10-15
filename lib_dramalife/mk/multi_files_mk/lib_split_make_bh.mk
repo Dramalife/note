1afc5e371418ca1de1642d75b02494dc27e2caa3
rc_ubuntu_vdev_x86 <dramalife@live.com>
2019-10-15 11:18:26 +0800

    Add WARNING && make --version .

--- lib_dramalife/mk/multi_files_mk/lib_split_make_bh.mk
+++ lib_dramalife/mk/multi_files_mk/lib_split_make_bh.mk
@@ -1,4 +1,38 @@
-# Ref : https://github.com/21cnbao/training/makefile/mul-dir-exam/Makefile
+# note "makefile" related file
+# Copyright (C) 2019 Dramalife@live.com
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# $ make --version
+# GNU Make 3.81
+# Copyright (C) 2006  Free Software Foundation, Inc.
+# This is free software; see the source for copying conditions.
+# There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.
+# 
+# This program built for i686-pc-linux-gnu
+# ;
+#
+# $ make --version
+# GNU Make 4.1
+# Built for x86_64-pc-linux-gnu
+# Copyright (C) 1988-2014 Free Software Foundation, Inc.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+# ;
+#
+# Ref : https://github.com/21cnbao/training/makefile/mul-dir-exam/Makefile ;
+#
+# Init : 2019.10.15;
+# Update :
+#
+
+
 
 SRC_DIR   :=$(CURRENT_DIR)/
 BUILD_DIR :=$(SRC_DIR)build/
