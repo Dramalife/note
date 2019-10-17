068c9e7fbcd08798a46a4b211a8ba63eb28b4cfc
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2019-10-17 09:16:30 +0000

    [source download] binutils-2.28, crosstool-ng-1.23.0, gcc-6.3.0, gcc-9.2.0, gettext-0.19.8.1, gmp-6.1.2, libiconv-1.15, mpc-1.0.3, mpfr-3.1.5, ncurses-6.0 .

---
+++ tools/00-source_code_of_tools/wget-libiconv-1.15.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#
+# note "crosstool-ng -- libiconv-1.15" related file
+# Copyright (C) 2019 Dramalife@live.com
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# bash --version
+# GNU bash, version 4.4.20(1)-release (x86_64-pc-linux-gnu)
+# Copyright (C) 2016 Free Software Foundation, Inc.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+# 
+# This is free software; you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+# ;
+#
+# Init : 2019.10.17;
+# Update :
+#
+
+
+wget https://mirrors.tuna.tsinghua.edu.cn/gnu/libiconv/libiconv-1.15.tar.gz
