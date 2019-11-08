4ea7ec233dd13109d98b035ebf72d75eb78be47b
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2019-11-08 06:43:43 +0000

    [audit-note] update autogen.sh, NOT FINISHED.

--- 80-userspace_programming/netlink/sample_NETLINK_AUDIT/autogen.sh
+++ 80-userspace_programming/netlink/sample_NETLINK_AUDIT/autogen.sh
@@ -10,9 +10,18 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
 # bash --version
-# TODO;
+# GNU bash, version 4.4.20(1)-release (x86_64-pc-linux-gnu)
+# Copyright (C) 2016 Free Software Foundation, Inc.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+# 
+# This is free software; you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+# ;
+#
+# TODO : Add symbol links to gitignore list! 
 #
 # Init : 2019.11.02;
+# Update : 2019.11.08;
 # Update :
 #
 
