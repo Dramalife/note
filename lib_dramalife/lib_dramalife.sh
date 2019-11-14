d709e6cb2c4dba6db74797750205cb12703d17e9
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2019-11-14 07:21:23 +0000

    [note-lib][shell] new version.

---
+++ lib_dramalife/lib_dramalife.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#
+# note "lib_dramalife" related file
+# Copyright (C) 2019 Dramalife@live.com
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# $ bash --version
+# GNU bash, version 4.4.20(1)-release (x86_64-pc-linux-gnu)
+# Copyright (C) 2016 Free Software Foundation, Inc.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+# 
+# This is free software; you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+# ;
+#
+# Init : 2019.11.14;
+# Update :
+#
+
+
+source print_lib/dramalife_terminal_color.sh
