b2fee538dcd263116323323968d36928f8172732
dramalife@dl_opi_armv7 <dramalife@live.com>
2019-11-18 22:55:42 +0800

    [note-lib][string_related_lib] init commit, (stage).

---
+++ lib_dramalife/string_related_lib/string_related_lib.c
@@ -0,0 +1,28 @@
+/*
+ * note "string_related_lib" related file
+ * Copyright (C) 2019 Dramalife <dramalife@live.com>
+ * 
+ * This file is part of [note](https://github.com/Dramalife/note.git)
+ * 
+ * note is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * $ gcc --version
+ * gcc (Debian 6.3.0-18+deb9u1) 6.3.0 20170516
+ * Copyright (C) 2016 Free Software Foundation, Inc.
+ * This is free software; see the source for copying conditions.  There is NO
+ * warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * ;
+ * 
+ * $ uname -a
+ * Linux orangepizero 4.19.62-sunxi #5.92 SMP Wed Jul 31 22:07:23 CEST 2019 armv7l GNU/Linux;
+ * 
+ * Init : 2019.11.18-22:27;
+ * Update 
+ *
+ */
+
+
+
+#include "string_related_lib.h"
