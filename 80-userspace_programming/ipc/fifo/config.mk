25ad3491dda690e229537703df428b1bb8209f23
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2019-12-07 09:16:37 +0000

    [fifo][named-pipe][ipc][80] init commit of fifo. src - TLPI.

---
+++ 80-userspace_programming/ipc/fifo/config.mk
@@ -0,0 +1,4 @@
+PATH_ABS=../../../lib_dramalife/
+include $(PATH_ABS)/mk/multi_lib.mk
+#CFLAGS+="-I$(PATH_ABS)"
+#DEF_MACROS +="-lpthread"
---
+++ 80-userspace_programming/ipc/fifo/readme.txt
@@ -0,0 +1,2 @@
+man 7 fifo
+fifo - first-in first-out special file, named pipe
