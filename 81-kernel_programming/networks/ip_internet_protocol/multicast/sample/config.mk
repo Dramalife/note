5416723b7d9df2b52c7e4ba2a0fda2760a5ae1a5
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2019-12-13 10:57:02 +0000

    stage .

--- 81-kernel_programming/networks/ip_internet_protocol/multicast/sample/config.mk
+++ 81-kernel_programming/networks/ip_internet_protocol/multicast/sample/config.mk
@@ -1,4 +1,5 @@
 PATH_ABS=../../../../../lib_dramalife/
 include $(PATH_ABS)/mk/multi_lib.mk
 CFLAGS+="-I$(PATH_ABS)"
+DO_NOT_USE_CFLAGS_IN_LIBMAKEFILE:=1
 #DEF_MACROS +="-lpthread"
--- 81-kernel_programming/networks/ip_internet_protocol/multicast/sample/multicast_server.c
+++ 81-kernel_programming/networks/ip_internet_protocol/multicast/sample/multicast_server.c
@@ -37,57 +37,101 @@
 #include <string.h>
 #include <netdb.h>
 #include <errno.h>
-#include "lib_dramalife.h"
+#include "lib_dramalife.h"	/* MAKE_GCC_HAPPY */
 
-#define BUFLEN 255
+
+/* Infile MACROS */
+#define BUFLEN 255			/* Buffer length of data of socket message */
+#define DEFAULT_MC_GRP "224.0.1.2"	/* Default IP multicast address of group */
+
 
 int main (int argc, char **argv)
 {
-	struct sockaddr_in peeraddr,ia;
+	int n;
 	int sockfd;
+	unsigned int socklen;
 	unsigned char loop;
 	char recmsg[BUFLEN + 1];
-	unsigned int socklen;
-	int n;
-	struct ip_mreq mreq;
+
+	/* In-function MACROS */
+	#define OPT_AUTO_CONFIG	0x0	/* Default - just demo */
+	int opt = OPT_AUTO_CONFIG;	
+	int ttl = 64;			/* TTL - Time To Live */
+
+	struct ip_mreq mreq;		/* Request struct for multicast socket ops */
+	struct sockaddr_in peeraddr,ia;	/* Structure describing an Internet (IP) socket address. */
 
 	MAKE_GCC_HAPPY((void *)&argc);
 	MAKE_GCC_HAPPY((void *)argv);
 
-	/* 创建 socket 用于UDP通讯 */
+	/* Create SOCKET - UDP */
 	sockfd = socket (AF_INET, SOCK_DGRAM, 0);
 	if (sockfd < 0)
 	{
 		printf ("socket creating err in udptalk\n");
 		exit (1);
 	}
-	/* 设置要加入组播的地址 */
+
 	bzero(&mreq, sizeof (struct ip_mreq));
 
-	inet_pton(AF_INET,"224.0.1.2",&ia.sin_addr);
-	/* 设置组地址 */
-	bcopy (&ia.sin_addr.s_addr, &mreq.imr_multiaddr.s_addr, sizeof (struct in_addr));
-	/* 设置发送组播消息的源主机的地址信息 */
-	mreq.imr_interface.s_addr = htonl (INADDR_ANY);
+	/* Setting
+	 * IP multicast address of group
+	 *
+	 */
+	switch(argc)
+	{
+		case 3:
+			opt = atoi(*(argv + 2));
+		case 2:
+			inet_pton(AF_INET,*(argv + 1),&ia.sin_addr);
+			break;
+		case 1:
+			inet_pton(AF_INET,DEFAULT_MC_GRP,&ia.sin_addr);
+			printf("Using default mc_addr ! \n"  "ARG1:MC_ADDR, ARG2:OPT \n");
+			break;
+		default:
+			break;
+	}
 
-	/* 把本机加入组播地址，即本机网卡作为组播成员，只有加入组才能收到组播消息 */
+	/* Setting Request struct for multicast socket ops */
+	bcopy (&ia.sin_addr.s_addr, &mreq.imr_multiaddr.s_addr, sizeof (struct in_addr));	/* Dest(Multicast) addr */
+	mreq.imr_interface.s_addr = htonl (INADDR_ANY);						/* Source addr */
 
+	/* IGMP - report */
 	if (setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP , &mreq,sizeof (struct ip_mreq)) == -1)
 	{
 		perror ("setsockopt");
 		exit (-1);
 	}
+
+	/* IGMP - leave */
+//	if (setsockopt(sockfd, IPPROTO_IP, IP_DROP_MEMBERSHIP , &mreq,sizeof (struct ip_mreq)) == -1)
+//	{
+//		perror ("setsockopt");
+//		exit (-1);
+//	}
+
 	loop = 0; 
 	if (setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_LOOP, &loop,sizeof (loop)) == -1)
 	{
 		printf("IP_MULTICAST_LOOP set fail!\n");
 	}
 
+	/* Setting - TTL */
+	if (setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_TTL, (char *)&ttl, sizeof(ttl)) == -1)
+	{
+		perror ("set TTL");
+	}
+
 	socklen = sizeof (struct sockaddr_in);
 	memset (&peeraddr, 0, socklen);
 	peeraddr.sin_family = AF_INET;
 	peeraddr.sin_port = htons (7838);
-	inet_pton(AF_INET, "224.0.1.2", &peeraddr.sin_addr); 
+
+	if(argc < 2)
+		inet_pton(AF_INET, DEFAULT_MC_GRP, &peeraddr.sin_addr); 
+	else
+		inet_pton(AF_INET,(char *)(argv + 1),&peeraddr.sin_addr); 
 
 	/* 绑定自己的端口和IP信息到socket上 */
 	if (bind(sockfd, (struct sockaddr *) &peeraddr,sizeof (struct sockaddr_in)) == -1)
@@ -99,10 +143,19 @@ int main (int argc, char **argv)
 	/* 循环接收网络上来的组播消息 */
 	for (;;)
 	{
-		printf("Please input:\n");
-		bzero (recmsg, BUFLEN + 1);
-		if (fgets (recmsg, BUFLEN, stdin) == (char *) EOF)
-			exit (0);
+		if( OPT_AUTO_CONFIG == opt )
+		{
+			printf("Please input:\n");
+			bzero (recmsg, BUFLEN + 1);
+			if (fgets (recmsg, BUFLEN, stdin) == (char *) EOF)
+				exit (0);
+		}
+		else
+		{
+			sleep(opt);
+			strncpy(recmsg, "1234", 4);
+			printf("sending ... \n");
+		}
 
 		if (sendto(sockfd, recmsg, strlen (recmsg), 0, (struct sockaddr *) &peeraddr, sizeof ( peeraddr)) < 0)
 		{
@@ -110,18 +163,21 @@ int main (int argc, char **argv)
 			exit (3);
 		}
 
-		printf ("send ok\n");
-		bzero (recmsg, BUFLEN + 1);
-		n = recvfrom (sockfd, recmsg, BUFLEN, 0, (struct sockaddr *) &ia, &socklen);
-		if (n < 0)
+		if( OPT_AUTO_CONFIG == opt )
 		{
-			printf ("recvfrom err in udptalk!\n");
-			exit (4);
-		}
-		else{
-			/* 成功接收到数据报 */
-			recmsg[n] = 0;
-			printf ("recv:[%s]\n", recmsg);
+			printf ("send ok\n");
+			bzero (recmsg, BUFLEN + 1);
+			n = recvfrom (sockfd, recmsg, BUFLEN, 0, (struct sockaddr *) &ia, &socklen);
+			if (n < 0)
+			{
+				printf ("recvfrom err in udptalk!\n");
+				exit (4);
+			}
+			else{
+				/* 成功接收到数据报 */
+				recmsg[n] = 0;
+				printf ("recv:[%s]\n", recmsg);
+			}
 		}
 	}
 }
