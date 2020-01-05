afd278aaf08251a14969bce0e8b8de6c030b568e
dramalife_ubuntu14_vbox_home <dramalife@live.com>
2020-01-05 16:18:08 +0800

    Show config that used building current running kernel.

---
+++ tools/system_status/config_current_kernel.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+#
+# note "kernel config" related file
+# Copyright (C) 2019 Dramalife <dramalife@live.com>
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# bash --version
+# GNU bash, version 4.3.11(1)-release (i686-pc-linux-gnu)
+# Copyright (C) 2013 Free Software Foundation, Inc.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+# 
+# This is free software; you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+# ;
+#
+# Init : 2020.01.05;
+# Update :
+#
+
+
+
+# Show config that used building current running kernel.
+zcat /proc/config.gz
