92faeabfb9ba9b3992d827e56400b4d392352621
Dramalife_rpi_centos <dramalife@live.com>
2020-01-12 11:37:48 +0800

    [centos][CentOS 7] 1.bash completion; 2.make completion; 3.install busybox; 4.samba related.

---
+++ 90-operating_system/distro_centos/bash_completion.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# CentOS 6
+#sudo rpm -ivh https://mirrors.ustc.edu.cn/epel/epel-release-latest-6.noarch.rpm
+#sudo yum -y install bash-completion
+
+# CentOS 7
+sudo yum -y install bash-completion
---
+++ 90-operating_system/distro_centos/make_completion.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# 1. Dependence :
+#sudo ./bash_completion.sh
+
+# 2. Add completion script
+#	Q : Where is "make" come from?
+#	A : Same path in Ubuntu.(/usr/share/bash-completion/completions/make)
+cp make /usr/share/bash-completion/completions/
