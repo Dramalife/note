71b149512a4b752cbf63f6358f91cbfda6f700bb
Dramalife_rpi_centos <dramalife@live.com>
2020-01-12 13:46:09 +0800

    [note-lib][ipc] Disable debug print by default.

--- lib_dramalife/ipc_lib/config.mk
+++ lib_dramalife/ipc_lib/config.mk
@@ -1,4 +1,6 @@
 PATH_ABS=../../lib_dramalife/
 include $(PATH_ABS)/mk/multi_files_mk/lib_split_make.mk
 CFLAGS+=-I..
-CFLAGS+=-DDRAMALIFE_NOTE_IPC_LIB_PIPE_C_DEBUG_SW=1
+
+# Debug print of the library.
+#CFLAGS+=-DDRAMALIFE_NOTE_IPC_LIB_PIPE_C_DEBUG_SW=1
