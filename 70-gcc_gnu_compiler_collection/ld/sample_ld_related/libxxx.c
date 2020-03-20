c68d2eec7ca280428f5b2397f1343677ce61061a
dramalife_ubuntu18_rpi_r2 <dramalife@live.com>
2020-03-20 23:20:43 +0800

    [gcc][ld] Fix bug.

--- 70-gcc_gnu_compiler_collection/ld/sample_ld_related/libxxx.c
+++ 70-gcc_gnu_compiler_collection/ld/sample_ld_related/libxxx.c
@@ -3,6 +3,7 @@
  * Dramalife@live.com
  * Init : 2020.03.04
  * Update : 2020.03.17, Add _LIB_USE_VAR_DEFINED_BY_APP
+ * Update : 2020.03.20, Add _LIBXXX_USING_LIBYYY ;
  */
 
 /* shm related */
@@ -15,14 +16,19 @@
 #ifdef _LIB_USE_VAR_DEFINED_BY_APP
 extern int var_a;
 #endif
+
+#ifdef _LIBXXX_USING_LIBYYY
 extern void func_in_libyyy(void);
+#endif
 
 void func_in_libxxx(void)
 {
 	printf("File:%-12s,Func:%-14s,Line:%-4d. \n",__FILE__,__func__,__LINE__);
 	shm_open("abcd",O_RDWR, 0);
 
+#ifdef _LIBXXX_USING_LIBYYY
 	func_in_libyyy();
+#endif
 
 #ifdef _LIB_USE_VAR_DEFINED_BY_APP
 	printf("File:%-12s,Func:%-14s,Line:%-4d,Var:%d. \n",__FILE__,__func__,__LINE__,var_a);
