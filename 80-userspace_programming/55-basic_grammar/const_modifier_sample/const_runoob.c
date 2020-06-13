ea13afacd894942ee026206a899e662a2e20bd5a
dramalife_ubuntu18_AliCloud <dramalife@live.com>
2020-06-13 18:10:31 +0800

    [80][55][basic] Update sample of modifier - "const".

--- 80-userspace_programming/55-basic_grammar/const_modifier_sample/const_runoob.c
+++ 80-userspace_programming/55-basic_grammar/const_modifier_sample/const_runoob.c
@@ -6,8 +6,18 @@ Update : fix : '/n' > '\n'
 #include <stdlib.h>
 #include <stdio.h>
 
-/* p is a pointer to const char ;) */
+
+/*****
+ * p is a pointer to const char ;) 
+ * const char *p;
+ *            *p : p is a pointer (to)
+ * const char    : const char
+ * * * * * * * * * * * * * * * * * *
+ * Vaule of p can be changed;
+ * Memory pointed by p can not be changed;
+ */
 const char *p="hello";
+
 
 void ptr_point2newmem(const char **pp)
 {
@@ -21,6 +31,17 @@ void chg_ptr_vaule(const char **pp)
 	//(*pp)[1] = 'x';
 }
 
+/*****
+ * pp is a pointer to pointer to const char
+ * const char **pp;
+ *             *pp : pp is a pointer (to)
+ *            *    : pointer (to)
+ * const char      : const char
+ * * * * * * * * * * * * * * * * * *
+ * Vaule of pp can be changed;
+ * Vaule of *pp (Memory pointed by pp) can be changed;
+ * Memory pointed by *pp can not be changed;
+ */
 void chg_ptr_point(const char **pp)
 {
 	*pp=NULL;
