23aa49998fb1aa4dc5f737d0ee4e5cd0ca6f6170
dramalife_ubuntu18-server_rcvb <dramalife@live.com>
2020-07-13 11:15:19 +0800

    [70][static_library] Add sample.

---
+++ 70-gcc_gnu_compiler_collection/static_library_libx_a/sample_static_library/caculate.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "math.h"
+
+int main(void)
+{
+	printf("6-3=%d\n", sub(6,3));
+	printf("6+3=%d\n", add(6,3));
+	return 0;
+}
---
+++ 70-gcc_gnu_compiler_collection/static_library_libx_a/sample_static_library/math.h
@@ -0,0 +1,7 @@
+#ifndef _MATH_H_
+#define _MATH_H_
+
+extern int add(int a, int b);
+extern int sub(int a, int b);
+
+#endif
