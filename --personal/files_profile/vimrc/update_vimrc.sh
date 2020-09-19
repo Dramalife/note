bf6f1c8e4925ee24e7f4c1571dc802b52c5d66fe
dramalife_vb_u16d <dramalife@live.com>
2020-09-19 11:55:27 +0800

    [vimrc] Update installation script.

--- --personal/files_profile/vimrc/update_vimrc.sh
+++ --personal/files_profile/vimrc/update_vimrc.sh
@@ -1,4 +1,33 @@
 #! /bin/bash
-mv -v ~/.vimrc ~/.vimrc_backup888
+#####
+# dramalife@live.com
+#
+
+
+# Warning
+echo -e "\nBelow is the content of this script:"
+echo -e "\nRead CAREFULLY!"
+cat ${0}
+echo -e "\n\nPress any key to continue:"
+read tmp
+echo -e "\n\nPress any key again to continue:"
+read tmp
+
+
+# Check & Install vim
+dpkg -s vim >/dev/null 2>&1
+if [ $? != 0 ]
+then
+echo -e "\nInstallation check error, install now..."
+sudo apt-get -y install vim
+else
+echo -e "\nInstallation check passed."
+fi
+
+
+# Update "vimrc" of current user
+echo -e "\nBackup the original vimrc ..."
+mv -v ~/.vimrc ~/.vimrc_dl_note_backup
+echo -e "\nInstall new vimrc ..."
 cp -v ./vimrc ~/.vimrc
-echo -e "done\n"
+echo -e "\nDone\n"
