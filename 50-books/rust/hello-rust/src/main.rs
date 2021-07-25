196e41989700282f14857628fa5af143782b5433
Dramalife_NanoPiM4 <dramalife@live.com>
2021-07-25 18:28:05 +0800

    Rust: Update demo.

--- 50-books/rust/hello-rust/src/main.rs
+++ 50-books/rust/hello-rust/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+  println !("Hello, world!");
+
+  command_line();
+
+  let ret = func_arg_int(77, 88);
+  println !("ret={}", ret);
+}
+
+fn command_line() {
+  println !("------------------");
+  println !("1. Command line printing;");
+  println !("2. Is a func;");
+
+  let a = 66;
+  println !("{{");
+  println !("a is {0}, again is {0}", a);
+}
+
+fn func_arg_int(ax : i32, ay : i32)->i32 {
+  println !("------------------");
+  println !("1. Func with arg;");
+  println !("2. Func with ret;");
+
+  println !("ax={}", ax);
+  println !("ay={}", ay);
+
+  return ax + ay;
 }
