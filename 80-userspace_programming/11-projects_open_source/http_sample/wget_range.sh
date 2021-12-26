4a682e7a4e06574f75b5fd2212e8c5e627e9e8b4
dramalife_ubuntu18_AliCloud <dramalife@live.com>
2021-12-26 22:22:03 +0800

    [http][range] wget.
    Signed-off-by: dramalife_ubuntu18_AliCloud <dramalife@live.com>

---
+++ 80-userspace_programming/11-projects_open_source/http_sample/wget_range.sh
@@ -0,0 +1,28 @@
+# note "wget - range" related file
+# Copyright (C) 2019 Dramalife <dramalife@live.com>
+# 
+# This file is part of [note](https://github.com/Dramalife/note.git)
+# 
+# note is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# 
+# $ gcc --version
+# gcc (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0
+# Copyright (C) 2017 Free Software Foundation, Inc.
+# This is free software; see the source for copying conditions.  There is NO
+# warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# ;
+# 
+# $ uname -a
+# Linux iZ2zeeq1jai7e6qn7xqrnuZ 4.15.0-96-generic #97-Ubuntu SMP Wed Apr 1 03:25:46 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux
+# ;
+# 
+# Init : Sat Aug 7 20:08:24 CST 2021
+#  * ;
+# 
+# Update
+
+
+
+wget pic.netbian.com/uploads/allimg/190824/212516-15666531161ade.jpg --header="Range: bytes=0-100"
