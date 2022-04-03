516191ce092c7b6fe7b0cd1695d49e9c89708b35
dramalife_ubuntu18_AliCloud <dramalife@live.com>
2022-04-03 22:57:13 +0800

    [dbus] Demo build: Header of x86_64.
    Signed-off-by: dramalife_ubuntu18_AliCloud <dramalife@live.com>

--- lib_dramalife/dbus_lib/dbus.mk
+++ lib_dramalife/dbus_lib/dbus.mk
@@ -9,11 +9,14 @@ env_mem=-fsanitize=address -fno-omit-frame-pointer
 env=-I /usr/include/dbus-1.0  \
 	-I /usr/lib/aarch64-linux-gnu/dbus-1.0/include/  \
 	-I /usr/lib/arm-linux-gnueabihf/dbus-1.0/include/  \
+	-I /usr/lib/x86_64-linux-gnu/dbus-1.0/include/ \
 	-I . \
+	-lpthread \
+	-D_DRAMALIFE_LIB_HAS_FUNC_MAIN_ \
 	-ldbus-1
 
 client:
-	gcc -o client.out \
+	$(CC) -o client.out \
 	client.c \
 	./init.c \
 	./method_call.c \
@@ -21,7 +24,7 @@ client:
 	$(env)
 
 server:
-	gcc -o server.out \
+	$(CC) -o server.out \
 	server.c \
 	./init.c \
 	./method_call.c \
