94d2710c71aa388ed8f7e206b522c79d99b6d044
Dramalife_OMV_HPGen8 <dramalife@live.com>
2022-04-05 01:35:41 +0800

    [python] Init.
    Signed-off-by: Dramalife_OMV_HPGen8 <dramalife@live.com>

---
+++ 50-books/python/arg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 2022/01 Init
+
+import sys
+
+argc=len(sys.argv)
+argv=str(sys.argv)
+print '参数个数为:', len(sys.argv), '个参数。'
+print '参数列表:', str(sys.argv)
