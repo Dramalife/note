e3e1c334bc6f11a37e8da72dc5c5bc7b7dde6054
dramalife_ubuntu18_AliCloud <dramalife@live.com>
2022-06-25 21:16:36 +0800

    Rust: Demo: Crates: json
    Signed-off-by: dramalife_ubuntu18_AliCloud <dramalife@live.com>

---
+++ 50-books/rust/crates/json_demo/readme.txt
@@ -0,0 +1,2 @@
+jo project=$(jo -a 1024 4096) > 1.json
+jo project=$(jo inc=1024 src=4096) > 1.json
--- 50-books/rust/crates/json_demo/src/main.rs
+++ 50-books/rust/crates/json_demo/src/main.rs
@@ -1,3 +1,12 @@
+use json;
+//use std::fs;
 fn main() {
+    //let content = fs::read_to_string("./1.json").unwrap();
+    let content = String::from(r#"{"project":{"inc":1024,"src":4096}}"#);
+    let parse = json::parse(content.as_str()).unwrap();
+    println!("  {}", parse["project"]);
+    println!("  {}", parse["project"][0]);
+    println!("  {}", parse["project"]["src"]);
+
     println!("Hello, world!");
 }
