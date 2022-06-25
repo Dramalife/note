557a0864080bc987004a0a6b0b900482dbd7b214
dramalife_ubuntu18_AliCloud <dramalife@live.com>
2022-06-25 21:17:52 +0800

    Rust: Demo: Crates: toml
    Signed-off-by: dramalife_ubuntu18_AliCloud <dramalife@live.com>

--- 50-books/rust/crates/toml_demo/src/main.rs
+++ 50-books/rust/crates/toml_demo/src/main.rs
@@ -1,3 +1,25 @@
+use std;
+use toml;
+use serde_derive::Deserialize;
+
+
+#[derive(Deserialize)]
+struct Config {
+    package: Package
+}
+#[derive(Deserialize)]
+struct Package {
+    name: String,
+    version: String,
+    edition: String,
+}
 fn main() {
+    let content = std::fs::read_to_string("Cargo.toml").unwrap();
+    let config: Config = toml::from_str(content.as_str()).unwrap();
+
+    println!("  name:{}", config.package.name);
+    println!("  version:{}", config.package.version);
+    println!("  edition:{}", config.package.edition);
+
     println!("Hello, world!");
 }
